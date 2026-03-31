dc950cb96bffc625f8a8671360ec9e8f190e613b
Udday Msakwa <uddaymsakwa@gmail.com>
2026-03-31 21:11:29 +0700

    Add application constants

---
+++ js/constants.js
@@ -0,0 +1,31 @@
+export const ROUTES = {
+    HOME: '/',
+    LOGIN: '/login',
+    REGISTRATION: '/registration',
+    PROFILE: '/profile',
+    CART: '/cart',
+    ORDERS: '/orders',
+    PURCHASE: '/purchase'
+};
+
+export const ORDER_STATUS = {
+    IN_PROCESS: 'InProcess',
+    DELIVERED: 'Delivered'
+};
+
+export const DISH_SORTING = {
+    NAME_ASC: 'NameAsc',
+    NAME_DESC: 'NameDesc',
+    PRICE_ASC: 'PriceAsc',
+    PRICE_DESC: 'PriceDesc',
+    RATING_ASC: 'RatingAsc',
+    RATING_DESC: 'RatingDesc'
+};
+
+export const DISH_CATEGORIES = [
+    'Wok',
+    'Pizza',
+    'Soup',
+    'Dessert',
+    'Drink'
+];
